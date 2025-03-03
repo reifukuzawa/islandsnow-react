83711600c6f69b3ae043ae907dabeea264298e8d
Rei <aloha.reirei1@gmail.com>
2025-03-02 22:02:24 -1000

    Task 2

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
+export default function Home() {
+  return (
+    <main>
+      <Container>
+        <h1 className="text-center">Island Snow!</h1>
+      </Container>
+    </main>
+  );
+}
